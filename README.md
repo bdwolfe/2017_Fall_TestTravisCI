91adbd131dcec85d8699920744b107e81645b214
Unknown <bdwolfe@users.noreply.github.com>
2017-08-15 16:35:31 -0400

    revise tests and add CI badge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 2017_Fall_TestTravisCI
 
+[![Build Status](https://travis-ci.org/bdwolfe/2017_Fall_TestTravisCI.svg?branch=master)](https://travis-ci.org/bdwolfe/2017_Fall_TestTravisCI)
+
 Trigger a build on TravisCI by adding this line
--- simple_tests.py
+++ simple_tests.py
@@ -2,21 +2,14 @@ import unittest
 import time
 
 class SimpleTests(unittest.TestCase):
-    def setUp(self):
-        self.x = 7
-        self.s = "a string"
 
-    def test_x_should_fail(self):
-        assert self.x == 9
     
-    def test_s_should_succeed(self):
-        assert self.y == "a string"
+    def test_should_succeed(self):
+        assert 7 == 7
         
     def test_old_print_syntax(self):
         print "This should succeed in Python 2.x, but not 3.x"
-        
-    def tearDown(self):
-        pass
+
         
 if __name__ == "__main__":
     unittest.main()
